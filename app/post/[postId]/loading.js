b5b566cdc22602462603737cdc95783103f10aef
ahmed barkhed <davidmohmed@gmail.com>
2025-03-01 12:09:48 +0400

    test

--- app/post/[postId]/loading.js
+++ app/post/[postId]/loading.js
@@ -7,7 +7,7 @@ export default function Loading() {
     <div className="flex flex-col items-center justify-center h-[80vh] bg-white">
       {/* الشعار */}
       <Image
-        src="globe.svg" 
+        src="/globe.svg" 
         alt="Loading..."
         width={150} 
         height={150}
